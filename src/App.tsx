00f967c1f1726ec51b6085b4e97a7cadaf41d251
BrayanRuiz <brayanja00@hotmail.com>
2025-04-14 17:43:25 -0600

    Initial Commit - Project React-Router

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+    return (
+        <>
+            <h1 className="text-3x1 font-bold underline">Hola Mundo</h1>
+            <Button variant="default">Click me</Button>
+        </>
+    )
+}
+
+export default App
