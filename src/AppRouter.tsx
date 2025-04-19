75339e3edebf8e0c3380e12df4e33fd1b7d39e7e
BrayanRuiz <brayanja00@hotmail.com>
2025-04-19 03:00:16 -0600

    ✨ feat(Lazy) : Se agrega la funcionalidad de carga perezosa

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,16 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+
 import AuthLayout from "./auth/layout/AuthLayout";
 import { RegisterPage } from "./auth/pages/RegisterPage";
 import { LoginPage } from "./auth/pages/LoginPage";
+import { sleep } from "./lib/sleep";
+
+// Lazy Load
+const ChatLayout = lazy(async () => {
+    await sleep(1500); 
+    return import("./chat/layout/ChatLayout");
+});
 
 export const AppRouter = () => {
     return (
@@ -11,6 +20,19 @@ export const AppRouter = () => {
                     <Route index element={<LoginPage />}/>
                     <Route path="/auth/register" element={<RegisterPage />}/>
                     {/* <Route path="/auth" element={<Navigate to="/auth/login" />} /> */}
+                </Route>
+
+                {/* Chat */}
+                <Route path="/chat" element={
+                    <Suspense fallback={
+                        <div className="flex justify-center items-center h-screen">
+                            <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
+                        </div>
+                    }>
+                        <ChatLayout />
+                    </Suspense>
+                }>
+
                 </Route>
                 
                 <Route path="*" element={<Navigate to="/auth" />} />
