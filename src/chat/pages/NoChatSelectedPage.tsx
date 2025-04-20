b63f9c680200bb48285bad125c3680ac45b708c7
BrayanRuiz <brayanja00@hotmail.com>
2025-04-20 13:13:52 -0600

    🔧 feat(Components) : Separación de componentes

--- src/chat/pages/NoChatSelectedPage.tsx
+++ src/chat/pages/NoChatSelectedPage.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const NoChatSelectedPage = () => {
     return (
-        <div>
-            <div className="flex flex-col items-center justify-center h-full gap-4">
-                <h1 className="text-2xl font-bold">No chat selected</h1>
-                <p className="text-muted-foreground">Please select a chat to start messaging.</p>
+        <div className="flex items-center justify-center h-screen">
+            <div className="flex flex-col items-center gap-4">
+            <h1 className="text-2xl font-bold">No chat selected</h1>
+            <p className="text-muted-foreground">Please select a chat to start messaging.</p>
             </div>
         </div>
     )
